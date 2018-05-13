7faed9c4a360adfa456f5ead1c33238961fe3aa5
Bekir <bekirshabut66@gmail.com>
2018-05-13 17:57:03 +0300

    finished game logic

--- js/fight.js
+++ js/fight.js
@@ -17,6 +17,9 @@ let blockButtons = document.querySelectorAll('.block');
 let controlsPnl = document.querySelector('.controls-pnl');
 let searchPnl = document.querySelector('.searching');
 let waitingPnl = document.querySelector('.waiting');
+let playerHp = document.querySelector('.your-hp');
+let enemyHp = document.querySelector('.enemy-hp');
+let statusPnl = document.querySelector('.status-pnl');
 
 const startFightBtn = document.querySelector('.start-fight');
 
@@ -65,32 +68,82 @@ turnBtn.onclick = function(){
     console.log(hit, blocks);
 
     let turn = {
-        hti: hit,
+        hit: hit,
         blocks: blocks
     }
 
     Ajax.post('http://localhost:3333/turn', {user_id: token, combat_id: combat.id, turn: JSON.stringify(turn)}, function(res){
         res = JSON.parse(res);
+        combat = res.combat;
+        if (res.combat.status === "finished") {
+            controlsPnl.classList.add('hidden');
+            waitingPnl.classList.add('hidden');
+            statusPnl.classList.remove('hidden');
+            if (res.combat.you.health > res.combat.enemy.health) {
+                statusPnl.textContent = "Вы победили!";
+            } else if (res.combat.you.health < res.combat.enemy.health) {
+                statusPnl.textContent = "Вы проиграли!";
+            } else {
+                statusPnl.textContent = "Ничья!";
+            }
+            return;
+        }
         if(res.combat.turn_status){
             playerHealth = res.combat.you.health;
             enemyHealth = res.combat.enemy.health;
+            playerHp.textContent = playerHealth;
+            enemyHp.textContent = enemyHealth;
+            let didHit = false;
+            res.combat.results[res.combat.results.length - 1].forEach(item => {
+                if(item.origin.id === token){
+                    didHit = !item.blocked;
+                }
+            });
+            let status = "Вы " + (didHit ? "попали!" : "не пробили блок!");
+            statusPnl.textContent = status;
+            statusPnl.classList.remove('hidden');
         } else {
             setTimeout(checkForOpponentMove, 1000);
             controlsPnl.classList.add('hidden');
             waitingPnl.classList.remove('hidden');
+            statusPnl.classList.add('hidden');
         }
     });
 }
 
 function checkForOpponentMove(){
-    Ajax.post('http://localhost:3333/status', {user_id: token, combat_id: combat.id}, function(res) {
+    Ajax.get('http://localhost:3333/status?user_id=' + token + "&combat_id=" + combat.id, function(res) {
         res = JSON.parse(res);
         combat = res.combat;
+        if (res.combat.status === "finished") {
+            controlsPnl.classList.add('hidden');
+            waitingPnl.classList.add('hidden');
+            statusPnl.classList.remove('hidden');
+            if (res.combat.you.health > res.combat.enemy.health) {
+                statusPnl.textContent = "Вы победили!";
+            } else if (res.combat.you.health < res.combat.enemy.health) {
+                statusPnl.textContent = "Вы проиграли!";
+            } else {
+                statusPnl.textContent = "Ничья!";
+            }
+            return;
+        }
         if(combat.turn_status) {
             waitingPnl.classList.add('hidden');
             controlsPnl.classList.remove('hidden');
             playerHealth = combat.you.health;
             enemyHealth = combat.enemy.health;
+            playerHp.textContent = playerHealth;
+            enemyHp.textContent = enemyHealth;
+            let didHit = false;
+            res.combat.results[res.combat.results.length - 1].forEach(item => {
+                if (item.origin.id === token) {
+                    didHit = !item.blocked;
+                }
+            });
+            let status = "Вы " + (didHit ? "попали!" : "не пробили блок!");
+            statusPnl.textContent = status;
+            statusPnl.classList.remove('hidden');
         } else {
             setTimeout(checkForOpponentMove, 1000);
         }
